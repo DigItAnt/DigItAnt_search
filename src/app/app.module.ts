84d9c6a9f5725bd7ea95fab93a3361885cdf521a
MicheleMallia <mallia.michele@gmail.com>
2022-10-06 17:58:55 +0200

    added flex, fontawesome, created minimal space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+//INTERNAL ANGULAR LIBS
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+//PRIMENG
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+
+//SHARED COMPONENTS
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
+import { MainComponent } from './shared/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ToolbarModule,
+    ButtonModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  filteredText: any[] = [];
+  items : any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 10000; i++) {
+        this.items.push({label: 'Item ' + i, value: 'Item ' + i});
+    }
+  }
+
+
+  filterText(event: any) {
+    let filtered: any[] = [];
+    let query = event.query;
+
+    if(this.items != undefined){
+      for (let i = 0; i < this.items.length; i++) {
+        let item = this.items[i];
+        if (item.label.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+          filtered.push(item);
+        }
+      }
+    }
+    
+    this.filteredText = filtered;
+
   }
 
 }
