097c1f0988f312711164ca162d1502a95e6e544f
MicheleMallia <mallia.michele@gmail.com>
2022-12-21 17:57:41 +0100

    fixed century pipe

--- src/app/pipes/century-pipe/century-pipe.pipe.ts
+++ src/app/pipes/century-pipe/century-pipe.pipe.ts
@@ -3,12 +3,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'centuryPipe'
 })
-export class CenturyPipePipe implements PipeTransform {
+export class CenturyPipe implements PipeTransform {
 
   transform(century: number): string {
     
     let beforeOrAfter : string = century < 0 ? 'BC' : 'AD';
     let absCentury : string = (Math.abs(century)/100).toString();
+    switch(absCentury){
+      case '1': absCentury = absCentury+"st";break;
+      case '2': absCentury = absCentury+"nd";break;
+      case '3': absCentury = absCentury+"rd";break;
+      default: absCentury = absCentury+"th"; break;
+    }
     return absCentury + ' century ' + beforeOrAfter;
   }
 
