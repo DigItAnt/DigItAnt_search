15a2aeaa0aaa689d09925b9140428a0e13763e10
MicheleMallia <mallia.michele@gmail.com>
2023-01-13 15:48:37 +0100

    refactor map service

--- src/app/services/maps/maps.service.ts
+++ src/app/services/maps/maps.service.ts
@@ -5,10 +5,10 @@ import { LocationsCounter } from 'src/app/views/texts/texts.component';
 import { environment } from 'src/environments/environment';
 
 export interface PleiadesDataModel {
-  bbox : Array<number>,
+  bbox: Array<number>,
   id: string,
   uri: string,
-  description : string,
+  description: string,
   reprPoint: Array<number>,
   title: string,
 }
@@ -36,7 +36,7 @@ export interface GeoNamesDataModel {
 export interface GlobalGeoDataModel {
   bbox: BBoxModel,
   id: string,
-  description : string,
+  description: string,
   reprPoint: Markers,
   title: string,
   uri: string,
@@ -53,54 +53,54 @@ export class MapsService {
   constructor(
     private http: HttpClient,
   ) { }
-  
-  mapPleiadesData(place : PleiadesDataModel) : GlobalGeoDataModel{
-    
+
+  mapPleiadesData(place: PleiadesDataModel): GlobalGeoDataModel {
+
     return {
-      bbox : place.bbox ? {north: place.bbox[2], south: place.bbox[0], west: place.bbox[1], east: place.bbox[3]} : {north: NaN, south: NaN, west: NaN, east: NaN},
-      description : place.description,
+      bbox: place.bbox ? { north: place.bbox[2], south: place.bbox[0], west: place.bbox[1], east: place.bbox[3] } : { north: NaN, south: NaN, west: NaN, east: NaN },
+      description: place.description,
       id: place.id,
-      reprPoint: place.reprPoint ? {latitude: place.reprPoint[1], longitude: place.reprPoint[0]} : {latitude: NaN, longitude: NaN},
+      reprPoint: place.reprPoint ? { latitude: place.reprPoint[1], longitude: place.reprPoint[0] } : { latitude: NaN, longitude: NaN },
       title: place.title,
       uri: place.uri,
     }
   }
 
-  mapGeoNamesData(place : GeoNamesDataModel, id: string, uri: string) : GlobalGeoDataModel{
+  mapGeoNamesData(place: GeoNamesDataModel, id: string, uri: string): GlobalGeoDataModel {
     return {
-      bbox : {north: place.bbox.north, south: place.bbox.south, west: place.bbox.west, east: place.bbox.east},
-      description : '',
+      bbox: { north: place.bbox.north, south: place.bbox.south, west: place.bbox.west, east: place.bbox.east },
+      description: '',
       id: id,
-      reprPoint: {latitude: parseFloat(place.lat), longitude: parseFloat(place.lng)},
+      reprPoint: { latitude: parseFloat(place.lat), longitude: parseFloat(place.lng) },
       title: place.name,
       uri: uri,
     }
   }
-  
+
   getGeoPlaceData(locations: LocationsCounter[]) {
 
     return forkJoin(
       locations.map(
         (req) => {
-          if((req.ancientPlaceId != 'unknown' && req.ancientPlaceId.length != 0)){
+          if ((req.ancientPlaceId != 'unknown' && req.ancientPlaceId.length != 0)) {
             return this.http.get(this.pleiadesBaseUrl + req.ancientPlaceId + '/json').pipe(
               catchError(err => of(err)),
               map(res => this.mapPleiadesData(res))
             )
           }
-        return this.http.get(
-          this.geoNamesBaseUrl + 
-          'getJSON?formatted=true&geonameId=' + 
-          req.modernPlaceId + 
-          '&username=mmallia92&style=full').pipe(
-            catchError(err => of(err)),
-            map(res => this.mapGeoNamesData(res, req.modernPlaceId, req.modernPlaceUrl))
-          );
+          return this.http.get(
+            this.geoNamesBaseUrl +
+            'getJSON?formatted=true&geonameId=' +
+            req.modernPlaceId +
+            '&username=mmallia92&style=full').pipe(
+              catchError(err => of(err)),
+              map(res => this.mapGeoNamesData(res, req.modernPlaceId, req.modernPlaceUrl))
+            );
         }
       )
     ).pipe(
-        catchError(err => of(err)),
-        map((results : GlobalGeoDataModel[]) => results.filter(element=> !isNaN(element.bbox.north)&& !isNaN(element.reprPoint.latitude) )),
+      catchError(err => of(err)),
+      map((results: GlobalGeoDataModel[]) => results.filter(element => !isNaN(element.bbox.north) && !isNaN(element.reprPoint.latitude))),
     )
   }
 }
